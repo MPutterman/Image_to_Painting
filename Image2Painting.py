1212a1c21d1bad8734b3448839325778f07e4cab
MPutterman <67725169+MPutterman@users.noreply.github.com>
2021-01-07 14:01:40 -0800

    Update Image2Painting.py

--- Image2Painting.py
+++ Image2Painting.py
@@ -4,6 +4,8 @@ class Drawer:
     def __init__(self):
         pass
     def drawit(self,img):
+        plt.imshow(img)
+        plt.show()
         edges = self.ideal_edges(img)
         patches = segmentation.watershed((edges))
         to_fill=self.fill_in(patches,img)
@@ -15,14 +17,15 @@ class Drawer:
     def likely_aliasing(self,to_fill,img):
         diff =abs(color.rgb2gray(img-to_fill))
         diff = diff>np.mean(diff)
-        diff = morphology.opening(diff,morphology.disk(3))
+        size=  len(img)*len(img[0])/200000
+        diff = morphology.opening(diff,morphology.disk(size))
         diff = filters.sobel(diff)>0
         return diff
     def fill_in(self,patches,img):
         num_colors = np.max(patches)
         patches-=1
         mi = np.min(patches)
-        new_arr = self.find_median(img,patches,num_colors)
+        new_arr = self.find_mean(img,patches,num_colors)
         to_fill =np.zeros_like(img)
         for i in range(len(patches)):
             for j in range(len(patches[0])):
@@ -35,9 +38,12 @@ class Drawer:
         im1=img[:,:,1]
         im2=img[:,:,2]
         im0=img[:,:,0]
+        tim = time.time()
+        
         i0=(np.digitize(im0,filters.threshold_multiotsu(im0,3)))
         i1=(np.digitize(im1,filters.threshold_multiotsu(im1,3)))
         i2=(np.digitize(im2,filters.threshold_multiotsu(im2,3)))
+        
         edges=(abs(filters.sobel(i0))+abs(filters.sobel(i1))+abs(filters.sobel(i2)))
         for i in range(len(img)):
             for j in range(len(img[i])):
@@ -45,7 +51,7 @@ class Drawer:
                     edges[i][j]=1
         edges=edges>0
         return edges
-    def find_median(self,img,edges,num_colors):
+    def find_mean(self,img,edges,num_colors):
         arr = [[] for i in range(num_colors)]
         for i in range(len(edges)):
             for j in range(len(edges[0])):
@@ -54,8 +60,8 @@ class Drawer:
         new_arr = [0 for i in range(num_colors)]
         for i in range(len(arr)):
             ar = np.array(arr[i])
-            median = np.array([np.median(ar[:,0]),np.median(ar[:,1]),np.median(ar[:,2])])
-            new_arr[i] = median
+            mean = np.array([np.mean(ar[:,0]),np.mean(ar[:,1]),np.mean(ar[:,2])])
+            new_arr[i] = mean
         return new_arr
 
 
